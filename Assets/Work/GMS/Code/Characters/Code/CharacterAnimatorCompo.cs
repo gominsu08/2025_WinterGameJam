e20df1a757c78953d7d7d6c961991b6a98c615e1
gominsu08 <minsu080618@gmail.com>
2025-12-22 12:40:53 +0900

    feat : PlayerAnimation + ...

---
+++ Assets/Work/GMS/Code/Characters/Code/CharacterAnimatorCompo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Work.Entities;
+
+namespace Work.Characters.Code
+{
+    public class CharacterAnimatorCompo : EntityAnimatorCompo
+    {
+        private Character _character;
+
+        public override void InitCompo(Entity entity)
+        {
+            base.InitCompo(entity);
+            _character = entity as Character;
+        }
+    }
+}
---
+++ Assets/Work/GMS/Code/Inputs/InputContainer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Work.Characters.Events;
+using Work.Utils.EventBus;
+
+namespace Work.Inputs
+{
+    public class InputContainer : Console.IPlayerActions
+    {
+        private Console _console;
+
+        public void Init()
+        {
+            if (_console == null)
+            {
+                _console = new Console();
+                _console.Player.SetCallbacks(this);
+            }
+            _console.Player.Enable();
+        }
+
+        ~InputContainer()
+        {
+            _console.Player.Disable();
+            _console = null;
+        }
+
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                Bus<CharacterInteractionEvent>.Raise(new CharacterInteractionEvent());
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            Vector2 vector = context.ReadValue<Vector2>();
+
+            Bus<CharacterMoveEvent>.Raise(new CharacterMoveEvent(new Vector3( vector.x,0, vector.y)));
+        }
+
+
+    }
+}
