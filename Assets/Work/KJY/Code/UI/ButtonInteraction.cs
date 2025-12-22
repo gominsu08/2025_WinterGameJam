b7797f6964266b0e1aa076ee86b4350ba08e3e00
Cwldb <mozi0810@gmail.com>
2025-12-22 19:06:49 +0900

    add : Title UIs

---
+++ Assets/Work/KJY/Code/UI/ButtonInteraction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+namespace Work.KJY.Code.UI
+{
+    public class ButtonInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private float duration = 0.2f;
+        [SerializeField] private float scaleFactor = 1.05f;
+
+        private Vector3 _defaultScale;
+        private Tween _currentTween;
+
+        private void Awake()
+        {
+            _defaultScale = transform.localScale;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _currentTween?.Kill();
+
+            _currentTween = transform.DOScale(_defaultScale * scaleFactor, duration).SetEase(Ease.OutQuad);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _currentTween?.Kill();
+
+            _currentTween = transform.DOScale(_defaultScale, duration).SetEase(Ease.OutQuad);
+        }
+    }
+}
---
+++ Assets/Work/KJY/Code/UI/TitleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Work.KJY.Code.Manager;
+
+namespace Work.KJY.Code.UI
+{
+    public class TitleController : MonoBehaviour
+    {
+        public void StartButton()
+        {
+            FadeManager.Instance.FadeIn(2, "Boost Item Test");
+        }
+
+        public void SettingButton()
+        {
+            
+        }
+
+        public void QuitButton()
+        {
+            Application.Quit();
+        }
+    }
+}
