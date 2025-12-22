90f9e7617369c189c0c5b298305e2fc6bdc4ce16
Cwldb <mozi0810@gmail.com>
2025-12-22 14:28:57 +0900

    add : Get Item Event

---
+++ Assets/Work/KJY/Code/Event/GetBoostItemEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Work.Utils.EventBus;
+
+public enum BoostType
+{
+    SpeedBoost,
+    SnowBoost,
+    SnowShield,
+    Other
+}
+
+namespace Work.KJY.Code.Event
+{
+    public class GetBoostItemEvent : IEvent
+    {
+        public BoostType Type;
+
+        public GetBoostItemEvent(BoostType type)
+        {
+            Type = type;
+        }
+    }
+}
--- Assets/Work/KJY/Code/Item/BoostChecker.cs
+++ Assets/Work/KJY/Code/Item/BoostChecker.cs
@@ -1,16 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Work.KJY.Code.Event;
+using Work.Utils.EventBus;
 
 namespace Work.KJY.Code.Item
 {
     public class BoostChecker : MonoBehaviour
     {
         [SerializeField] private LayerMask playerLayer;
-        
+        [SerializeField] private BoostType boostType;
+
         private void OnTriggerEnter(Collider other)
         {
             if ((playerLayer & (1 << other.gameObject.layer)) != 0)
             {
-                Debug.Log("asd");
+                Bus<GetBoostItemEvent>.Raise(new GetBoostItemEvent(boostType));
             }
         }
     }
