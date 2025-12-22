802465274896a737f5ae72b7879683cbb3757940
Cwldb <mozi0810@gmail.com>
2025-12-22 16:14:59 +0900

    add : Plaza Manager

---
+++ Assets/Work/KJY/Code/Manager/PlazaManager.cs
@@ -0,0 +1,22 @@
+﻿using AYellowpaper.SerializedCollections;
+using UnityEngine;
+using Work.KJY.Code.Core;
+
+namespace Work.KJY.Code.Manager
+{
+    public class PlazaManager : MonoSingleton<PlazaManager>
+    {
+        [SerializedDictionary("level", "Need Money")]
+        public SerializedDictionary<int, int> levelDict = new();
+        
+        private int _curLevel;
+
+        public int GetCurLevel() => _curLevel;
+        public int GetNeedMoney() => levelDict[_curLevel];
+
+        public void LevelUp()
+        {
+            _curLevel++;
+        }
+    }
+}
