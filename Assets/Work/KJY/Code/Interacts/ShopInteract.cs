87862107b430097605a9a775299938d8f95de927
Cwldb <mozi0810@gmail.com>
2025-12-23 12:06:40 +0900

    add : Shop Interact

---
+++ Assets/Work/KJY/Code/Interacts/ShopInteract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Work.Characters.Events;
+using Work.Utils.EventBus;
+
+namespace Work.KJY.Code.Interacts
+{
+    public class ShopInteract : MonoBehaviour
+    {
+        [SerializeField] private LayerMask playerMask;
+
+        private bool _isInPlayer;
+
+        private void Start()
+        {
+            Bus<CharacterInteractionEvent>.Events += OnInteractShop;
+        }
+
+        private void OnInteractShop(CharacterInteractionEvent evt)
+        {
+            if (_isInPlayer)
+            {
+                // 여기에 Shop UI 띄우면 될듯
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if ((playerMask & (1 << other.gameObject.layer)) != 0)
+                _isInPlayer = true;
+            else
+                _isInPlayer = false;
+        }
+    }
+}
