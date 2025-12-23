66daac395251dc7e95dc2376d5e7cc69387328be
gominsu08 <minsu080618@gmail.com>
2025-12-24 07:08:01 +0900

    fix :

--- Assets/Work/KJY/Code/UI/TreeUpgradeController.cs
+++ Assets/Work/KJY/Code/UI/TreeUpgradeController.cs
@@ -24,7 +24,6 @@ namespace Work.KJY.Code.UI
         
         private void Start()
         {
-            Inventory.Instance.AddMoney(100000);
             interactPanel.SetActive(false);
             Bus<TreeInteractEvent>.Events += OnCanInteract;
             Bus<PlazaLevelUpgradedEvent>.Events += OnPlazaLevelUpgraded;
