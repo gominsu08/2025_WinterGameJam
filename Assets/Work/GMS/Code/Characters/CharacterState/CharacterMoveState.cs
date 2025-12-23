b5d35d49d42aded2684d2ce69b59ad06dd3018ef
Cwldb <mozi0810@gmail.com>
2025-12-24 02:03:09 +0900

    add : Foot step

--- Assets/Work/GMS/Code/Characters/CharacterState/CharacterMoveState.cs
+++ Assets/Work/GMS/Code/Characters/CharacterState/CharacterMoveState.cs
@@ -1,20 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Work.Characters.Events;
 using Work.Entities;
+using Work.KJY.Code.Manager;
 
 namespace Work.GMS.Code.Characters.CharacterState
 {
     public class CharacterMoveState : CharacterCanMoveState
     {
-
         public CharacterMoveState(Entity entity, int animHash) : base(entity, animHash)
         {
+        }
+
+        public override void Enter()
+        {
+            base.Enter();
+            _trigger.OnSnowSoundEvent += SnowSound;
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            _trigger.OnSnowSoundEvent -= SnowSound;
+        }
+
+        private void SnowSound()
+        {
+            Debug.Log("SNOW");
+            SoundManager.Instance.Play2DSFX("STONE1"); // + Random.Range(1, 2).ToString());
         }
 
         protected override void MoveHandler(CharacterMoveEvent evt)
         {
             if (evt.MoveDirection == Vector3.zero)
             {
+                //SoundManager.Instance.
                 _stateCompo.ChangeState("IDLE");
             }
         }
